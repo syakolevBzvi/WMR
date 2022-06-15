71623f4357e19c79a591c087577ad7602c96f189
syakolevBzvi <107340690+syakolevBzvi@users.noreply.github.com>
2022-06-15 12:36:33 +0300

    Ruuner

--- app/src/main/java/Runner.java
+++ app/src/main/java/Runner.java
@@ -5,7 +5,30 @@ public class Runner
     protected String name;
     protected Date birtdate;
     protected String gender;
-    
 
+    public Runner(String name, Date birtdate, String gender) {
+        this.name = name;
+        this.birtdate = birtdate;
+        this.gender = gender;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirtdate() {
+        return birtdate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
